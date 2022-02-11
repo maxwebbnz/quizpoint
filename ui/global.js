7091092a692919ef64cf3ebd899db27438c64316
Alan McIlwaine <alan.j.mcilwaine@gmail.com>
2022-02-11 23:05:51 +1300

    Made the classroom cards on home screen look a bit prettier

--- ui/global.js
+++ ui/global.js
@@ -23,17 +23,36 @@ function closeNav() {
 
 let classList = [
     {
-        "teacher": "Mr Alan",
+        "classroom": "9PTEC",
         "image": "https://st2.depositphotos.com/6672578/10091/i/600/depositphotos_100912942-stock-photo-woodwork-workshop-wall-with-many.jpg",
+        "teacher": "Alan McIlwaine",
     },
     {
-        "teacher": "Mr Max",
+        "classroom": "13COMP",
         "image": "https://media.istockphoto.com/photos/field-full-of-green-grass-and-hills-picture-id146766798?k=20&m=146766798&s=612x612&w=0&h=UQulU1Miuimu96r1yApRgBiwXcUGgeUhFgH3kuHWOEc=",
+        "teacher": "Max Webb",
     },
     {
-        "teacher": "Mr Gillies",
+        "classroom": "13DTEC",
         "image": "https://blogs.windows.com/wp-content/uploads/prod/2020/08/windows-logo-social.png",
-    }
+        "teacher": "Steven Leishman",
+    },
+    {
+        "classroom": "13PHY",
+        "image": "https://media.istockphoto.com/photos/field-full-of-green-grass-and-hills-picture-id146766798?k=20&m=146766798&s=612x612&w=0&h=UQulU1Miuimu96r1yApRgBiwXcUGgeUhFgH3kuHWOEc=",
+        "teacher": "Bryan Gillies",
+    },
+    {
+        "classroom": "13DRA",
+        "image": "https://media.istockphoto.com/photos/field-full-of-green-grass-and-hills-picture-id146766798?k=20&m=146766798&s=612x612&w=0&h=UQulU1Miuimu96r1yApRgBiwXcUGgeUhFgH3kuHWOEc=",
+        "teacher": "Katie Long",
+    },
+    {
+        "classroom": "13ELE",
+        "image": "https://media.istockphoto.com/photos/field-full-of-green-grass-and-hills-picture-id146766798?k=20&m=146766798&s=612x612&w=0&h=UQulU1Miuimu96r1yApRgBiwXcUGgeUhFgH3kuHWOEc=",
+        "teacher": "Jason Tierney",
+    },
+
 ];
 
 let classCardArray;
@@ -50,16 +69,21 @@ let classroomContainer = {
         let cardBody = document.createElement("div");
         let cardTitle = document.createElement("p");
         let cardImg = document.createElement("img");
-        card.className = "card g-col-6 g-col-md-4";
+        let cardFooter = document.createElement("div");
+        card.className = "card g-col-6 g-col-md-4 ";
         cardBody.className = "card-body";
-        cardTitle.className = "card-title";
-        cardTitle.innerText = classroom.teacher + "'s Classroom";
+        cardTitle.className = "card-title text-center";
+        cardTitle.innerText = classroom.classroom;
         cardImg.src = classroom.image;
-        cardImg.className = "card-img-top";
+        cardImg.className = "card-img-top rounded";
+        cardFooter.className ="card-footer text-muted";
+        cardFooter.innerText = classroom.teacher;
 
         cardBody.appendChild(cardImg);
         cardBody.appendChild(cardTitle);
+        cardBody.appendChild(cardFooter);
         card.appendChild(cardBody);
         classCardArray.appendChild(card);
+        console.log(classCardArray)
     },
 }

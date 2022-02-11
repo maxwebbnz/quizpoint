6310b0ad6d70fbc592a0eb2c63afc75d50bfb2c1
Max Webb <maxwebblighting@gmail.com>
2022-02-11 17:06:57 +1300

    added some new files, file management and working on nav bar - thanks to w3

---
+++ ui/global.js
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2022 Safety 360 (Alan and Max)
+ * All rights reserved.
+ */
+/*
+ * Copyright (c) 2022 Safety 360 (Alan and Max)
+ * All rights reserved.
+ */
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("sidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("sidenav").style.width = "0";
+}/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("sidenav").style.width = "0";
+}

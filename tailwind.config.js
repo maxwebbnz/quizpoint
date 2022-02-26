b9b222d21f3e7647cd35a1081a70d55e7105aa33
MJW <60947369+maxwebbnz@users.noreply.github.com>
2022-02-27 10:14:43 +1300

    Install tailwind.css but it is a bit confusing,

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2022 Max Webb
+ * All rights reserved.
+ */
+module.exports = {
+    content: ["./public/**/*.{html,js}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}

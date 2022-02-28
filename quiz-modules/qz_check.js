c6afeff0d4701fe02301bc84913f3181cbc369a8
MJW <60947369+maxwebbnz@users.noreply.github.com>
2022-03-01 12:53:29 +1300

    Camel Casing, Keywords in an workable state.

--- quiz-modules/qz_check.js
+++ quiz-modules/qz_check.js
@@ -14,26 +14,43 @@ let qz_check = {
                 return false
             }
         } else if (_t == "textinput") {
-            if (currentQuiz[currentQuestion].keywords == "") {
-
-            }
             let lowerCaseA = _a.toLowerCase()
-            let wordsCorrect = 0;
             let lcans = currentQuiz[currentQuestion].answer
             let ansToCheck = lcans.toLowerCase()
             let answerSplit = ansToCheck.split(' ')
-            console.log(answerSplit)
-            for (var i = 0; i < answerSplit.length; i++) {
-                console.log(lowerCaseA.includes(answerSplit[i]))
-                if (lowerCaseA.includes(answerSplit[i])) {
-                    wordsCorrect = wordsCorrect + 1;
+
+            if (Array.isArray(currentQuiz[currentQuestion].keywords)) {
+                console.log("Question is a keyword based")
+                let keywordsCorrect = 0;
+                for (var i = 0; i < currentQuiz[currentQuestion].keywords.length; i++) {
+                    let kywrd = currentQuiz[currentQuestion].keywords[i]
+                    console.log(kywrd)
+                    if (lowerCaseA.includes(kywrd)) {
+                        keywordsCorrect = wordsCorrect + 1;
+                    }
+                }
+                if (keywordsCorrect >= currentQuiz[currentQuestion].minKeyWords) {
+                    return true
+                } else {
+                    return false
+                }
+            } else {
+                // console.log(answerSplit)
+                let wordsCorrect = 0;
+                if (wordsCorrect >= currentQuiz[currentQuestion].minKeyWords) {
+                    return true
+                } else {
+                    return false
+                }
+                for (var i = 0; i < answerSplit.length; i++) {
+                    console.log(lowerCaseA.includes(answerSplit[i]))
+                    if (lowerCaseA.includes(answerSplit[i])) {
+                        wordsCorrect = wordsCorrect + 1;
+                    }
                 }
             }
-            if (wordsCorrect >= 1) {
-                return true
-            } else {
-                return false
-            }
+
+
         }
     }
 }

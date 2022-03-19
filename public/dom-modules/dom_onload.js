35f655bf3115f6c2281d818a929d5ca02f68f504
MJW <60947369+maxwebbnz@users.noreply.github.com>
2022-03-19 19:45:56 +1300

    Dialog added to protect program from refresh events.
    Although, this will not stop users refresh if they want to, only a alert

--- public/dom-modules/dom_onload.js
+++ public/dom-modules/dom_onload.js
@@ -6,6 +6,9 @@
 $(document).ready(function() {
     fb_init()
         // $('#studentInfoModal').modal('show');
-    // $("#tcs_createQuizModal").modal("show");
+        // $("#tcs_createQuizModal").modal("show");
+    window.onbeforeunload = function() {
+        return "This program will restart, which will cause you to navigate back to the home page and lose all progress.";
+    }
 
 })

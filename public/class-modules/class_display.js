1a6338ce77184485bee3b6e2d9d96a6e2195727f
Alan McIlwaine <alan.j.mcilwaine@gmail.com>
2022-03-24 13:29:14 +1300

    Drawing chart.js, not displaying but code is written

--- public/class-modules/class_display.js
+++ public/class-modules/class_display.js
@@ -110,5 +110,29 @@ cls.display = {
         qz.loadActive.match(user.uid, _classId)
             // navigate to class page for student
         ui.navigate.to('classpage_authed_student')
+    },
+    loadQuizProgressBar: function(){
+        let ctx = document.getElementById('quizProgress').getContext('2d');
+        let quizData = {
+            labels:[
+                'Turned In',
+                'Not Completed',
+            ],
+            datasets: [{
+                data: [10, 5],
+                backgroundColor: [
+                    'rgb(80, 200, 120)',
+                    'rgb(238, 75, 43)',
+                ],
+                hoverOffset: 3
+            }]
+        };
+        let quizChart = new Chart(ctx, {
+            type: 'doughnut',
+            data: quizData,
+        });
+        quizCjart
+
     }
 }
+    

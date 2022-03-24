efb1e38f51e118caba4c285c5cc03287a694b75e
Alan McIlwaine <alan.j.mcilwaine@gmail.com>
2022-03-25 10:56:56 +1300

    ts.loadClassPage.loadQuizProgressBar() to display progress bar, it now fits properly

--- public/teachingsuite-modules/ts_loadClassPage.js
+++ public/teachingsuite-modules/ts_loadClassPage.js
@@ -2,15 +2,11 @@ ts.loadClassPage = {
     loadQuizProgressBar: function() {
         let ctx = document.getElementById('quizProgress').getContext('2d');
         let quizData = {
-            labels: [
-                'Turned In',
-                'Not Completed',
-            ],
             datasets: [{
                 data: [10, 5],
                 backgroundColor: [
                     'rgb(80, 200, 120)',
-                    'rgb(238, 75, 43)',
+                    'rgb(211,211,211)',
                 ],
                 hoverOffset: 3
             }]
@@ -18,6 +14,10 @@ ts.loadClassPage = {
         let quizChart = new Chart(ctx, {
             type: 'doughnut',
             data: quizData,
+            options: {
+                responsive:true,
+                maintainAspectRatio:false,
+            },
         });
     }
 }

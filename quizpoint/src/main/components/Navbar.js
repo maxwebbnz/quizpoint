85d43bbdd0808d1ed6ac429905f312e852ad56f4
Alan McIlwaine <alan.j.mcilwaine@gmail.com>
2022-04-06 11:46:23 +1200

    Navbar looks prettier with drop shadows and hover effects

--- quizpoint/src/main/components/Navbar.js
+++ quizpoint/src/main/components/Navbar.js
@@ -10,17 +10,17 @@ import logo from './logo.svg'
 const NavBar = () => {
     return (
         <Navbar expand="lg">
-                <Navbar.Brand className = "navbar-brand" href=""><img src={logo} width="40" height="40" alt="QuizPoint Logo"/>QuizPoint</Navbar.Brand>
+                <Navbar.Brand className = "navbar-brand" href="/"><img src={logo} width="40" height="40" alt="QuizPoint Logo"/>QuizPoint</Navbar.Brand>
                 <Nav className="navbar-links">
                     {/* Normal view --Note, dissapears once screen is small */}
-                    <Nav.Link className="" href="">Your Classes</Nav.Link>
-                    <Nav.Link className="" href="">Teaching Home (Test)</Nav.Link>
+                    <Nav.Link className="" href="/classHome">Your Classes</Nav.Link>
+                    <Nav.Link className="" href="/tcs">Teaching Home (Test)</Nav.Link>
                     {/* Dropdown is hidden until screen size is small */}
                     <Dropdown className="navbar-dropdown">
                         <Dropdown.Toggle className="navbar-dropdown-toggle"></Dropdown.Toggle>
-                        <Dropdown.Menu className="position-absolute dropdown-menu-end">
-                            <Dropdown.Item className="">Your Classes</Dropdown.Item>
-                            <Dropdown.Item className="">Teaching Home</Dropdown.Item>
+                        <Dropdown.Menu className="navbar-dropdown-menu position-absolute dropdown-menu-end">
+                            <Dropdown.Item href="/classHome" className="">Your Classes</Dropdown.Item>
+                            <Dropdown.Item href="/tcs" className="">Teaching Home</Dropdown.Item>
                         </Dropdown.Menu>
                     </Dropdown>
                 </Nav>

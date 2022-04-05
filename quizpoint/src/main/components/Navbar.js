b7129859588a014d14e25311f3f7a06d6ef6e26d
Alan McIlwaine <alan.j.mcilwaine@gmail.com>
2022-04-06 11:21:31 +1200

    Navbar is now responsive with screen size

--- quizpoint/src/main/components/Navbar.js
+++ quizpoint/src/main/components/Navbar.js
@@ -3,20 +3,28 @@
  * All rights reserved.
  */
 
-import {Navbar, Nav, Container} from 'react-bootstrap';
+import {Navbar, Nav, Dropdown} from 'react-bootstrap';
 import './NavBar.css';
 import logo from './logo.svg'
 
 const NavBar = () => {
     return (
-        <Navbar bg="light" expand="lg">
-            <Container className="navbar-container">
+        <Navbar expand="lg">
                 <Navbar.Brand className = "navbar-brand" href=""><img src={logo} width="40" height="40" alt="QuizPoint Logo"/>QuizPoint</Navbar.Brand>
                 <Nav className="navbar-links">
-                    <Nav.Link className="navbar-links-classes" href="">Your Classes</Nav.Link>
+                    {/* Normal view --Note, dissapears once screen is small */}
+                    <Nav.Link className="" href="">Your Classes</Nav.Link>
+                    <Nav.Link className="" href="">Teaching Home (Test)</Nav.Link>
+                    {/* Dropdown is hidden until screen size is small */}
+                    <Dropdown className="navbar-dropdown">
+                        <Dropdown.Toggle className="navbar-dropdown-toggle"></Dropdown.Toggle>
+                        <Dropdown.Menu className="position-absolute dropdown-menu-end">
+                            <Dropdown.Item className="">Your Classes</Dropdown.Item>
+                            <Dropdown.Item className="">Teaching Home</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
                 </Nav>
-            </Container>
-      </Navbar>
+        </Navbar>
     )
 }
 

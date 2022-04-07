3adc508c4647ce84c5e25d2ae8ca2ab5c0afd12f
MJW <60947369+maxwebbnz@users.noreply.github.com>
2022-04-08 10:25:48 +1200

    Protected displaying of links of Navbar

--- quizpoint/src/main/components/Navbar.js
+++ quizpoint/src/main/components/Navbar.js
@@ -6,8 +6,13 @@
 import {Navbar, Nav, Dropdown} from 'react-bootstrap';
 import './NavBar.css';
 import logo from './logo.svg'
+// import {user} from '.'
+import {user} from '../../firebase/fb.user'
+
 
 const NavBar = () => {
+    // if role is teacher, display link to page
+    if(user.role === 'teacher'){
     return (
         <Navbar expand="lg">
                 <Navbar.Brand className = "navbar-brand" href="/"><img src={logo} width="40" height="40" alt="QuizPoint Logo"/>QuizPoint</Navbar.Brand>
@@ -27,6 +32,29 @@ const NavBar = () => {
                 </Nav>
         </Navbar>
     )
+    }else{
+        // else do not they cannot access it
+    return (
+        <Navbar expand="lg">
+                <Navbar.Brand className = "navbar-brand" href="/"><img src={logo} width="40" height="40" alt="QuizPoint Logo"/>QuizPoint</Navbar.Brand>
+                <Nav className="navbar-links">
+                    {/* Normal view --Note, dissapears once screen is small */}
+                    <Nav.Link className="" href="/classHome">Your Classes</Nav.Link>
+                    {/* <Nav.Link className="" href="/tcs">Teaching Home (Test)</Nav.Link> */}
+                    <Nav.Link className="" href="/logout">Logout</Nav.Link>
+                    {/* Dropdown is hidden until screen size is small */}
+                    <Dropdown className="navbar-dropdown">
+                        <Dropdown.Toggle className="navbar-dropdown-toggle"></Dropdown.Toggle>
+                        <Dropdown.Menu className="navbar-dropdown-menu position-absolute dropdown-menu-end">
+                            <Dropdown.Item href="/classHome" className="">Your Classes</Dropdown.Item>
+                            {/* <Dropdown.Item href="/tcs" className="">Teaching Home</Dropdown.Item> */}
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </Nav>
+        </Navbar>
+    )
+    }
+
 }
 
 export default NavBar

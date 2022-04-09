bf9e2e2b8e01c6c30ce8ab57bed8a61d6283d567
MJW <60947369+maxwebbnz@users.noreply.github.com>
2022-04-09 14:37:16 +1200

    404 page look change

--- quizpoint/src/main/404.js
+++ quizpoint/src/main/404.js
@@ -2,9 +2,20 @@
  * Copyright (c) 2022 QuizPoint
  * All rights reserved.
  */
+
+import './errorpage.css'
+import { Image, Button } from 'react-bootstrap'
 const errorPageNotFound = () => {
-    return(
-        <h2>Sorry, no page here!</h2>
+    return (
+        <div className="error-page">
+            <div className='branding'>
+                <Image src="media/branding/appicon-itt6.svg" width='100'></Image>
+            </div>
+            <div className="error">
+                <h2><b>404 | Not Found</b></h2>
+                <Button href="/">Return Home</Button>
+            </div>
+        </div>
     )
 }
 

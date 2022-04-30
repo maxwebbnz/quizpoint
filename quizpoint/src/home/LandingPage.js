7081b62115474cd4bec53cf25fe12b2acd3b64cc
MJW <60947369+maxwebbnz@users.noreply.github.com>
2022-04-30 16:20:47 +1200

    Adding redirects for netlify

--- quizpoint/src/home/LandingPage.js
+++ quizpoint/src/home/LandingPage.js
@@ -10,10 +10,10 @@ import logo from './media/appicon-itt6.svg'
 import mobileLogo from './media/appbranding-itt4withtext.svg'
 import schoolMedia from './media/background.jpg'
 import { useMediaQuery } from 'react-responsive'
-
+import { useState, useEffect } from 'react';
 // Material UI
 import Backdrop from '@mui/material/Backdrop';
-
+import Fade from '@mui/material/Fade';
 
 /**==============================================
  **              LandingPage
@@ -25,13 +25,34 @@ export default function LandingPage() {
     const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
     const shouldBackdrop = true
 
+    const MyFade = ({
+        children,
+        in: In = true,
+        timeout = 1000,
+        delay = 0
+    }) => {
+        const [isIn, setIsIn] = useState(In && delay === 0)
+
+        useEffect(() => {
+            if (delay > 0) {
+                setTimeout(() => setIsIn(true), delay)
+            }
+        })
+
+        return (
+            <Fade in={isIn} timeout={timeout}>
+                {children}
+            </Fade>
+        )
+    }
 
     if (isBigScreen || isDesktopOrLaptop) {
         // return HTML
         return (
             <div id="landingPage" class="homePageMain">
                 <div class="backgroundImage-landingpage">
-                    <img src={schoolMedia} alt=" " id="schoolMedia-background" width="100%" height="100%"></img>
+                    <MyFade in timeout={{ enter: 2000 }} delay={100}><img src={schoolMedia} alt=" " id="schoolMedia-background" width="100%" height="100%"></img></MyFade>
+
                     <Backdrop
                         sx={{ color: '#b5b5b5', zIndex: (theme) => theme.zIndex.drawer + 1 }}
                         open={shouldBackdrop}

4d1a64558c1501dcc5afb3999b8cd596fe10ccdb
MJW <60947369+maxwebbnz@users.noreply.github.com>
2022-05-14 21:07:01 +1200

    Starting on StudentReport page again.

--- quizpoint/src/teachingsuite/reportingfeatures/StudentReport.js
+++ quizpoint/src/teachingsuite/reportingfeatures/StudentReport.js
@@ -18,7 +18,7 @@ import autoTable from 'jspdf-autotable';
 import { useTable } from 'react-table'
 
 // database
-import { db } from '../../services/firebase'
+import { db, dbFunctions } from '../../services/firebase'
 // components from libs
 import { ref, onValue } from "firebase/database";
 // mui
@@ -34,10 +34,16 @@ import Tooltip from '@mui/material/Tooltip';
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 import * as htmlToImage from 'html-to-image';
 import MUIDataTable from "mui-datatables";
+import PropTypes from 'prop-types';
+import Typography from '@mui/material/Typography';
 
 import HashLoader from "react-spinners/HashLoader";
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
+
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 import AssignmentLateOutlinedIcon from '@mui/icons-material/AssignmentLateOutlined';
 import AssignmentTurnedInOutlinedIcon from '@mui/icons-material/AssignmentTurnedInOutlined';
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
@@ -61,116 +67,43 @@ export default function StudentReport() {
     let [color, setColor] = useState("#ffffff");
     // just a placeholder variable
     const shouldFade = true
+    const [tabs, setTabs] = useState([])
     //use effect hook, no reference
+    const [age, setAge] = useState('');
+    const [quizToSelect, setSelect] = useState([])
+    const handleChange = (event) => {
+        setAge(event.target.value);
+        console.log(event.target.value)
+    };
+
     useEffect(() => {
-        // if program is loading
         if (loading) {
-            // echo to title
-            document.title = 'Loading Student | QuizPoint'
-            /**======================
-             **   LoadStudents
-            *? Loads student data from firebase and is the brains of module
-            *========================**/
-            function loadStudent() {
-                // reference to student id
-                let pathRef = ref(db, `/schools/hvhs/users/${id}`)
-                //on value
-                onValue(pathRef, (snapshot) => {
-                    // if snapshot undefined
-                    if (snapshot.val() === undefined || snapshot.val() === null) {
-                        console.log("invalid student id")
-                    } else {
-                        // set student object
-                        let studentObject = {
-                            uid: snapshot.val().uid,
-                            name: snapshot.val().name,
-                            quizzes: snapshot.val().quizzes,
-                        }
-                        let studentArray = [studentObject.name]
+            document.title = 'Loading Data | QuizPoint'
+            let pathRef = ref(db, `/schools/hvhs/users/${id}`);
 
-                        let quizCodeToDisplay = []
-                        // load quiz information
-                        let quizCodes = snapshot.val().quizzes.active
-                        for (const [key, value] of Object.entries(quizCodes)) {
-                            quizCodeToDisplay.push(key)
-                        }
-                        console.log(quizCodeToDisplay)
-                        for (var index = 0; index < quizCodeToDisplay.length; index++) {
-
-                        }
-                        // set loading to false
-                        setLoading(false)
+            onValue(pathRef, (snapshot) => {
+                console.log(snapshot.val())
+                let quizReference = snapshot.val().quizzes.active
+                console.log(quizReference)
+                let optionArray = []
+                for (const property in quizReference) {
+                    console.log(`${property}: ${quizReference[property].name}`);
+                    optionArray.push({ name: quizReference[property].name, id: property, code: quizReference[property].code })
+                }
 
 
-                    }
-
+                let quizSelect = optionArray.map((quiz) => {
+                    return (
+                        <MenuItem value={quiz.code}>{quiz.name}</MenuItem>
+                    )
                 })
-            }
-            // execute load student
-            loadStudent()
+                setSelect(quizSelect)
+                console.log(quizToSelect)
+                setLoading(false)
+            })
+
         }
     })
-    // window.locatio  n.reload(false);
-    const columns = useMemo(
-        () => [
-            {
-                Header: 'Student Information',
-                columns: [
-                    {
-                        Header: 'Name',
-                        accessor: 'name',
-                    },
-                    {
-                        Header: 'Quiz Code',
-                        accessor: 'code',
-                    },
-
-                ],
-            },
-            {
-                Header: 'Quiz',
-                columns: [
-                    {
-                        Header: '1',
-                        accessor: 'question1',
-                    },
-                    {
-                        Header: '2',
-                        accessor: 'question2',
-                    },
-                    {
-                        Header: '3',
-                        accessor: 'question3',
-                    },
-                    {
-                        Header: '4',
-                        accessor: 'question4',
-                    },
-                ],
-            },
-        ],
-        []
-    )
-
-    const data = [{
-        name: "Max Webb",
-        question1: "Correct",
-        question2: "Correct",
-        question3: "Correct",
-        question4: "Correct"
-    }, {
-        name: "Maxwell Webb",
-        question1: "Correct",
-        question2: "Correct",
-        question3: "Correct",
-        question4: "Correct"
-    },
-    ]
-
-    const tableData = useMemo(() => currentTableData)
-
-    console.log(data)
-
     function Table({ columns, data }) {
         // Use the state and functions returned from useTable to build your UI
         const {
@@ -211,6 +144,8 @@ export default function StudentReport() {
             </table>
         )
     }
+
+
     // if loading
     if (loading) {
         return (
@@ -226,65 +161,27 @@ export default function StudentReport() {
             </div>
         )
     } else {
-        let noDoubleUpData = [...new Set(tableData)];
 
-
-        let userLoaded = {
-            uid: "adadawd",
-            name: "Max Webb",
-            email: '18205mw@hvhs.school.nz',
-            studentID: '18205mw',
-            picture: 'https://lh3.googleusercontent.com/a-/AOh14Gi4yHlhKDaUDCvUxS_ZgS9OdjYN-bEPabU8kLrm3Q=s96-c',
-        }
-
-
-
-        console.log(currentTableData)
         return (
-            <div className="student-report">
-                <div className="student-report-studentinfo">
-                    <div className="banner-details">
-                        {/* Banner 1 - Details */}
-                        <h5><InfoOutlinedIcon></InfoOutlinedIcon> Personal Details</h5>
-                    </div>
-                    <div className="user-content">
-                        <div className="user-content-left">
-                            {/* User Profile Picture */}
-                            <Tooltip title="Image taken from students google account">
-                                {/* On image hover, message displayed */}
-                                <img alt='User profile' src={userLoaded.picture}></img>
-                            </Tooltip>
-                        </div>
-                        <div className="user-content-right">
-                            {/* Basic Student information */}
-                            <p>Name: {userLoaded.name}</p>
-                            <p>Student ID: {userLoaded.studentID}</p>
-                            {/* when you click on link, it will send email */}
-                            <p>Email: <a href={'mailto:' + userLoaded.email}>{userLoaded.email}</a></p>
-                        </div>
-                    </div>
+            <div className='student-report'>
+                <div className='student-report-container'>
+                    <Box sx={{ minWidth: 120 }}>
+                        <FormControl fullWidth>
+                            <InputLabel id="demo-simple-select-label">Quiz</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                                value={age}
+                                label="Quiz"
+                                onChange={handleChange}
+                            >
+                                {quizToSelect}
+                            </Select>
+                        </FormControl>
+                    </Box>
                 </div>
-                <div className="student-report-container">
-                    <div className="student-report-header">
-                        <h5>Quizzes</h5>
-                        <Tabs>
-                            <TabList>
-                                <Tab>Select a tab</Tab>
-                                <Tab><AssignmentLateOutlinedIcon></AssignmentLateOutlinedIcon> Health and Safety Pt 1.</Tab>
-                                <Tab><AssignmentTurnedInOutlinedIcon></AssignmentTurnedInOutlinedIcon> Workplace Safety Pt 2</Tab>
+                <div className='student-report-table'>
 
-                            </TabList>
-                            <TabPanel>
-                                <h2>Select a tab to view quic information</h2>
-                            </TabPanel>
-                            <TabPanel>
-                                <div id="report">
-                                    <Table columns={columns} data={noDoubleUpData} />
-
-                                </div>
-                            </TabPanel>
-                        </Tabs>
-                    </div>
                 </div>
             </div>
         )

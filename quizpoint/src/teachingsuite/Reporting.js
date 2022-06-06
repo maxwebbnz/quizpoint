7f0cb8c609b4374e2dfd63fc5caa981cc48d8aed
MJW <60947369+maxwebbnz@users.noreply.github.com>
2022-06-06 19:49:39 +1200

    Reworking /reporting/ home page. Both student and class features done, all use autocomplete...

--- quizpoint/src/teachingsuite/Reporting.js
+++ quizpoint/src/teachingsuite/Reporting.js
@@ -2,173 +2,252 @@
  * Copyright (c) 2022 QuizPoint
  * All rights reserved.
  */
+/**========================================================================
+ * *                          Reporting Module
+ *========================================================================**/
+
+/**======================
+ **   React Imports
+ *========================**/
 import React, { useState, useEffect } from 'react'
 import { useParams, useNavigate } from "react-router-dom"
 
-// database
+/**======================
+ **   Data Service Imports
+ *========================**/
 import { db } from '../services/firebase'
-
-// components from libs
 import { ref, onValue } from "firebase/database";
-import './Reporting.css'
+/**======================
+ **   Material UI Imports
+ *========================**/
+
 import Button from '@mui/material/Button';
-import ButtonGroup from '@mui/material/ButtonGroup';
-import SchoolOutlinedIcon from '@mui/icons-material/SchoolOutlined';
-import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
-import QuizOutlinedIcon from '@mui/icons-material/QuizOutlined';
 import Backdrop from '@mui/material/Backdrop';
-import CircularProgress from '@mui/material/CircularProgress';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import Autocomplete from '@mui/material/Autocomplete';
-import Slide from '@mui/material/Slide';
-
 import TextField from '@mui/material/TextField';
-import Stack from '@mui/material/Stack';
 import HashLoader from "react-spinners/HashLoader";
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
 
+/**======================
+ **   Stylesheet Imports
+ *========================**/
+import './Reporting.css'
+/**==============================================
+ **              Reporting()
+*?  What does it do? Hub for reporting, home page
+*=============================================**/
 export default function Reporting() {
-    let { field } = useParams()
-    const [loadingData, isLoading] = useState(true)
-    let [color, setColor] = useState("#ffffff");
-    const [options, setOptions] = useState([])
+    // bool placeholder
     const shouldFade = true
+    // color for backdrop
+    let [color] = useState("#ffffff");
+    // states
+    const [loadingData, isLoading] = useState(true)
     const [classes, setClasses] = useState([])
     const [students, setStudents] = useState([])
-    const [open, setOpen] = useState(false);
+    const [studentOptions, setStudentOptions] = useState([])
+    const [classOptions, setClassOptions] = useState([])
+    const [studentOpen, setStudentOpen] = useState(false);
+    const [classOpen, setClassOpen] = useState(false);
     const [selectedOption, setOption] = useState('')
-    const [setOfOptions, setSetOfOptions] = useState([])
+    // navigate reference
     let navigate = useNavigate()
-    let selectedClassId
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
-
-    const handleClose = () => {
-        setOpen(false);
-    };
-    const handleSelect = () => {
-        navigate('/tcs/reports/class/' + selectedClassId)
-        setOpen(false);
-    };
-
-    const Transition = React.forwardRef(function Transition(props, ref) {
-        return <Slide direction="up" ref={ref} {...props} />;
-    });
 
 
+    // use effect hook
     useEffect(() => {
+        // if data is loading
         if (loadingData) {
-            document.title = 'Loading... | QuizPoint'
-            function loadAllClasses() {
-                let pathRef = ref(db, `/schools/hvhs/classes/`);
+            /**======================
+             **   Load Data
+             *? What does it do? Loads data from firebase
+             *========================**/
+            function loadData() {
+                // path reference
+                const pathRef = ref(db, `/schools/hvhs/users/`);
+                // wait for data
                 onValue(pathRef, (snapshot) => {
-                    if (snapshot === undefined || snapshot === null) {
-                        console.log("invalid class code")
-                    } else {
-                        snapshot.forEach(childSnapshot => {
-                            if (childSnapshot.cache) {
-                                return;
-                            } else {
-                                classes.push(childSnapshot.val())
+                    // for each user
+                    snapshot.forEach(childSnapshot => {
+                        // if user is a student
+                        if (childSnapshot.val().role === "student") {
+                            // add student to student array
+                            setStudents(prevStudents => [...prevStudents, childSnapshot.val()])
+                            // create an option
+                            let newStudent = {
+                                label: childSnapshot.val().name,
+                                value: childSnapshot.key
                             }
+                            // add option to student options
+                            studentOptions.push(newStudent)
+                        }
+
+                    })
+                    // then load classes
+                    const pathRef2 = ref(db, `/schools/hvhs/classes/`);
+                    // wait for data
+                    onValue(pathRef2, (snapshot) => {
+                        // for each class
+                        snapshot.forEach(childSnapshot => {
+                            // add class to class array
+                            setClasses(prevClasses => [...prevClasses, childSnapshot.val()])
+                            // create an option
+                            let newClass = {
+                                label: childSnapshot.val().className,
+                                value: childSnapshot.key
+                            }
+                            // add option to class options
+                            classOptions.push(newClass)
                         })
-                        console.log(classes)
-                    }
+                    })
                 })
             }
-            loadAllClasses()
+            // load data
+            loadData()
+            // set loading to false
+            isLoading(false)
 
-            function loadAllStudents() {
-                let pathRef = ref(db, `/schools/hvhs/users/`);
-                onValue(pathRef, (snapshot) => {
-                    if (snapshot === undefined || snapshot === null) {
-                        console.log("invalid class code")
-                    } else {
-                        snapshot.forEach(childSnapshot => {
-                            if (childSnapshot.cache) {
-                                return;
-                            } else {
-                                if (childSnapshot.val().role !== "student") {
-                                    console.log('not a student')
-                                } else {
-                                    // error prevention
-                                    //! this code does not work as intended, sucks to be me!
-                                    if (childSnapshot.val().average === undefined || childSnapshot.val().average === null) {
-                                        childSnapshot.val().average = 0
-                                        students.push(childSnapshot.val())
-                                    } else {
-                                        students.push(childSnapshot.val())
-
-                                    }
-
-                                }
-                            }
-                        })
-                        isLoading(false)
-                        console.log(students)
-                    }
-                })
-
-            }
-            loadAllStudents()
+            // if data is not loading
         } else {
-            document.title = 'HELLO'
-        }
-    })
-
-
-    function displaySelectionDialog(type) {
-        let selectedArray;
-
-        if (type === "class") {
-            selectedArray = classes
-            let placeholder = classes.map(classObj => {
-                return (
-                    <MenuItem key={classObj.code} value={classObj.code}>
-                        {classObj.className}
-                    </MenuItem>
-                )
-            })
-            setSetOfOptions(placeholder)
-        } else if (type === "student") {
-            selectedArray.type = students
-
-            let placeholder = students.map(classObj => {
-                return (
-                    <MenuItem key={classObj.key} value={classObj.uid}>
-                        {classObj.name}
-                    </MenuItem>
-                )
-            })
-            setSetOfOptions(placeholder)
-            setOption(type)
-
-            // after options setting,
-            setOpen(true);
+            // set title
+            document.title = 'Reports | QuizPoint'
 
         }
+    }, [loadingData, classes, students])
 
 
-        setOption(type)
+    /**==============================================
+     **              studentDialog
+     *?  What does it do? Opens a dialog for selecting a student
+     *=============================================**/
+    function studentDialog() {
 
-        // after options setting,
-        setOpen(true);
+        /**======================
+         **   handleClose()
+        *?  What does it do? Closes the dialog
+         *========================**/
+        const handleClose = () => {
+            // close dialog
+            setStudentOpen(false);
+            // if field is empty
+            if (selectedOption === '') {
+                // set selected option to empty
+                setStudentOpen(false);
 
+                // else if not
+            } else {
+                // navigate to report page
+                navigate('/tcs/reports/student/' + selectedOption)
+            }
+        };
+
+        // return JSX dialog
+        return (
+            <Dialog
+                open={studentOpen}
+                onClose={handleClose}
+                fullWidth={true}
+
+                maxWidth={'md'}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">
+                    {"Select a Student"}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        <FormControl fullWidth>
+                            <Autocomplete
+                                onChange={(event, newValue) => {
+                                    setOption(newValue.value)
+                                }}
+                                getOptionLabel={option => option.label}
+                                disablePortal
+                                id="combo-box-demo"
+                                options={studentOptions}
+                                renderInput={(params) => <TextField {...params} label="Student" />}
+                            />
+                        </FormControl>
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} autoFocus>
+                        Select
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        )
     }
 
-    function handleClassSelect(e) {
-        console.log(e.target.value)
-        selectedClassId = e.target.value
-    }
+    /**==============================================
+     *           classDialog
+     * ?  What does it do? Opens a dialog for selecting a class
+     * ==============================================**/
+    function classDialog() {
+        /**======================
+         **   handleClose
+        *?  What does it do? Closes the dialog
+         *========================**/
 
+        const handleClose = () => {
+            // close dialog
+            setClassOpen(false);
+            // if field is empty
+            if (selectedOption === '') {
+
+                setClassOpen(false);
+                // else if not
+            } else {
+                // navigate to report page
+                navigate('/tcs/reports/class/' + selectedOption)
+            }
+        };
+
+        // return JSX dialog
+        return (
+            <Dialog
+                open={classOpen}
+                onClose={handleClose}
+                fullWidth={true}
+
+                maxWidth={'md'}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">
+                    {"Select a class"}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        <FormControl fullWidth>
+                            <Autocomplete
+                                onChange={(event, newValue) => {
+                                    setOption(newValue.value)
+                                }}
+                                getOptionLabel={option => option.label}
+                                disablePortal
+                                id="combo-box-demo"
+                                options={classOptions}
+                                renderInput={(params) => <TextField {...params} label="Class" />}
+                            />
+                        </FormControl>
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} autoFocus>
+                        Select
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        )
+    }
+    // if data is loading
     if (loadingData) {
         return (
             <div className="loading-container">
@@ -181,42 +260,18 @@ export default function Reporting() {
                 </Backdrop>
             </div>
         )
+
+        // if data is not loading
     } else {
         return (
-            <div>
+            <div className='reporting'>
                 <h1>Select a reporting feature {loadingData}</h1>
                 <div className="reporting-buttons">
-                    <Button variant="contained" color="primary" onClick={() => displaySelectionDialog('class')}>View by class</Button>
-                    <Button variant="contained" color="primary" onClick={() => console.log('clickedf')}>View by quiz</Button>
-                    <Button variant="contained" color="primary" onClick={() => displaySelectionDialog('student')}>View by student</Button>
+                    <button className='generic-button sml' onClick={() => setStudentOpen(true)}>Student</button>
+                    <button className='generic-button sml' onClick={() => setClassOpen(true)}>Class</button>
                 </div>
-                <Dialog
-                    open={open}
-                    TransitionComponent={Transition}
-                    keepMounted
-                    onClose={handleClose}
-                    aria-describedby="alert-dialog-slide-description"
-                >
-                    <DialogTitle>Select a {selectedOption}</DialogTitle>
-                    <DialogContent>
-                        <DialogContentText id="alert-dialog-slide-description">
-                            <FormControl fullWidth>
-                                <InputLabel id="demo-simple-select-label">{selectedOption}</InputLabel>
-                                <Select
-                                    labelId="demo-simple-select-label"
-                                    id="demo-simple-select"
-                                    label="Age"
-                                    onChange={handleClassSelect}
-                                >
-                                    {setOfOptions}
-                                </Select>
-                            </FormControl>
-                        </DialogContentText>
-                    </DialogContent>
-                    <DialogActions>
-                        <Button onClick={handleSelect}>Select</Button>
-                    </DialogActions>
-                </Dialog>
+                {studentDialog()}
+                {classDialog()}
             </div >
 
         )
